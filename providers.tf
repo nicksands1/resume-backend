aee9611962777ecc1037e817a0251af57c1291d9
nicksands1 <nicksanders41@gmail.com>
2022-11-29 08:36:42 -0600

    providers

---
+++ providers.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "nicksands-state"
+    key = "cloudresume/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region                   = "us-east-1"
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "nick"
+}
+
+provider "aws" {
+  alias = "acm_provider"
+  region = "us-east-1"
+}
