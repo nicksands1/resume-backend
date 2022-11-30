90930868342ee6a5b6e77e1a039055814cb25fb7
nicksands1 <nicksanders41@gmail.com>
2022-11-30 15:45:19 -0600

    update lambda function

--- python/add_visits.py
+++ python/add_visits.py
@@ -1,33 +1,19 @@
-
-
 import json
 import boto3
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('visit_table')
-
 def lambda_handler(event, context):
-
     response = table.get_item(Key={
-       'record_id':'visitors'
+            'record_id':'0'
     })
-    
     visitor_count = response['Item']['visitor_count']
     visitor_count = visitor_count + 1
     print(visitor_count)
-
     response = table.put_item(Item={
-            'record_id':'visitors',
+            'record_id':'0',
             'visitor_count': visitor_count
-    })
+    }
+    return "Records added successfully!"
+    )
     
-    response = {
-      'statusCode': 200,
-      'body': visitor_count,
-      'headers' : {
-        'Access-Control-Allow-Origin': '*'
-      }
-              
-            }
-  
-    return response
