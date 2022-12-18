30e6de3afec48401f08b0ab6ac7eb7e899bedff3
nicksands1 <nicksanders41@gmail.com>
2022-12-18 07:25:58 -1000

    redo api

--- terraform/api.tf
+++ terraform/api.tf
@@ -1,181 +1,113 @@
-resource "aws_api_gateway_rest_api" "MyDemoAPI" {
-  name        = "MyDemoAPI"
-  description = "This is my API for demonstration purposes"
+resource "aws_api_gateway_rest_api" "crc_api" {
+  name        = "crc_api"
+  description = "API to get data from DynamoDB via Lambda"
   endpoint_configuration {
     types = ["REGIONAL"]
   }
 }
 
-resource "aws_api_gateway_resource" "MyDemoResource" {
-  rest_api_id = aws_api_gateway_rest_api.MyDemoAPI.id
-  parent_id   = aws_api_gateway_rest_api.MyDemoAPI.root_resource_id
-  path_part   = "MyDemoResource"
+
+resource "aws_api_gateway_resource" "crc_api" {
+  parent_id   = aws_api_gateway_rest_api.crc_api.root_resource_id
+  path_part   = "crc_api"
+  rest_api_id = aws_api_gateway_rest_api.crc_api.id
 }
 
-resource "aws_api_gateway_method" "opt" {
-  rest_api_id   = aws_api_gateway_rest_api.MyDemoAPI.id
-  resource_id   = aws_api_gateway_resource.MyDemoResource.id
-  http_method   = "OPTIONS"
+# module "cors" {
+#   source = "squidfunk/api-gateway-enable-cors/aws"
+#   version = "0.3.3"
+#   api_id          = aws_api_gateway_rest_api.crc_api.id
+#   api_resource_id = aws_api_gateway_resource.crc_api.id
+# }
+
+resource "aws_api_gateway_method" "crc_api" {
   authorization = "NONE"
- 
+  http_method   = "GET"
+  resource_id   = aws_api_gateway_resource.crc_api.id
+  rest_api_id   = aws_api_gateway_rest_api.crc_api.id
 }
 
-resource "aws_api_gateway_integration" "opt" {
-  rest_api_id = aws_api_gateway_rest_api.MyDemoAPI.id
-  resource_id = aws_api_gateway_resource.MyDemoResource.id
-  http_method = aws_api_gateway_method.opt.http_method
-  type = "MOCK"
+resource "aws_api_gateway_integration" "crc_api" {
+  http_method             = aws_api_gateway_method.crc_api.http_method
+  resource_id             = aws_api_gateway_resource.crc_api.id
+  rest_api_id             = aws_api_gateway_rest_api.crc_api.id
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.test_lambda.invoke_arn
 }
 
-resource "aws_api_gateway_integration_response" "opt" {
-  rest_api_id = aws_api_gateway_rest_api.MyDemoAPI.id
-  resource_id = aws_api_gateway_resource.MyDemoResource.id
-  http_method = aws_api_gateway_method.opt.http_method
-  status_code = 200
+resource "aws_api_gateway_method_response" "response_200" {
+  depends_on = [
+    aws_api_gateway_method.crc_api
+  ]
+  rest_api_id = aws_api_gateway_rest_api.crc_api.id
+  resource_id = aws_api_gateway_resource.crc_api.id
+  http_method = aws_api_gateway_method.crc_api.http_method
+  status_code = "200"
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = "'*'",
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Requested-With'",
-    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'"
-  }
-depends_on = [
-  aws_api_gateway_integration.opt,aws_api_gateway_method.opt
-]
-}
-
-resource "aws_api_gateway_method_response" "opt" {
-  rest_api_id = aws_api_gateway_rest_api.MyDemoAPI.id
-  resource_id = aws_api_gateway_resource.MyDemoResource.id
-  http_method = aws_api_gateway_method.opt.http_method
-  status_code = 200
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true,
     "method.response.header.Access-Control-Allow-Methods" = true,
     "method.response.header.Access-Control-Allow-Headers" = true
   }
   response_models = {
     "application/json" = "Empty"
   }
-  depends_on = [
-    aws_api_gateway_method.opt
-  ]
-}
-resource "aws_api_gateway_method" "MyDemoMethod" {
-  rest_api_id   = aws_api_gateway_rest_api.MyDemoAPI.id
-  resource_id   = aws_api_gateway_resource.MyDemoResource.id
-  http_method   = "GET"
-  authorization = "NONE"
 }
 
-
-
-
-resource "aws_api_gateway_integration" "integration" {
-  rest_api_id             = aws_api_gateway_rest_api.MyDemoAPI.id
-  resource_id             = aws_api_gateway_resource.MyDemoResource.id
-  http_method             = aws_api_gateway_method.MyDemoMethod.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.test_lambda.invoke_arn
-   
+resource "aws_api_gateway_integration_response" "cors" {
+  depends_on  = [aws_api_gateway_integration.crc_api, aws_api_gateway_method_response.response_200]
+  rest_api_id = aws_api_gateway_rest_api.crc_api.id
+  resource_id = aws_api_gateway_resource.crc_api.id
+  http_method = aws_api_gateway_method.crc_api.http_method
+  status_code = 200
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'", # replace with hostname of frontend (CloudFront)
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET, POST'" # remove or add HTTP methods as needed
+  }
 }
 
+resource "aws_api_gateway_deployment" "crc_api" {
+  rest_api_id = aws_api_gateway_rest_api.crc_api.id
 
-resource "aws_lambda_permission" "apigw_lambda" {
-  statement_id  = "AllowExecutionFromAPIGateway"
+  triggers = {
+    # NOTE: The configuration below will satisfy ordering considerations,
+    #       but not pick up all future REST API changes. More advanced patterns
+    #       are possible, such as using the filesha1() function against the
+    #       Terraform configuration file(s) or removing the .id references to
+    #       calculate a hash against whole resources. Be aware that using whole
+    #       resources will show a difference after the initial implementation.
+    #       It will stabilize to only change when resources change afterwards.
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.crc_api.id,
+      aws_api_gateway_method.crc_api.id,
+      aws_api_gateway_integration.crc_api.id,
+    ]))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "crc_api" {
+  deployment_id = aws_api_gateway_deployment.crc_api.id
+  rest_api_id   = aws_api_gateway_rest_api.crc_api.id
+  stage_name    = "crc_api"
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.test_lambda.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn = "${aws_api_gateway_rest_api.MyDemoAPI.execution_arn}/*/*/*"
-  
-}
 
-resource "aws_api_gateway_method_response" "response_200" {
-  rest_api_id = aws_api_gateway_rest_api.MyDemoAPI.id
-  resource_id = aws_api_gateway_resource.MyDemoResource.id
-  http_method = aws_api_gateway_method.MyDemoMethod.http_method
-  status_code = "200"
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = true,
-    "method.response.header.Access-Control-Allow-Methods" = true,
-    "method.response.header.Access-Control-Allow-Headers" = true
-    
-  }
-  
-  response_models = {
-   "application/json" : "Empty"
-  }
-}
-
-resource "aws_api_gateway_integration_response" "MyDemoIntegrationResponse" {
-  rest_api_id = aws_api_gateway_rest_api.MyDemoAPI.id
-  resource_id = aws_api_gateway_resource.MyDemoResource.id
-  http_method = aws_api_gateway_method.MyDemoMethod.http_method
-  status_code = aws_api_gateway_method_response.response_200.status_code
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = "'*'",
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Requested-With'",
-    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'"
-  }
-  depends_on = [
-    aws_api_gateway_integration.integration
-  ]
-}
-
-resource "aws_api_gateway_gateway_response" "response_4xx" {
-  rest_api_id   = aws_api_gateway_rest_api.MyDemoAPI.id
-  response_type = "DEFAULT_4XX"
-
-  response_templates = {
-    "application/json" = "{'message':$context.error.messageString}"
-  }
-
-  response_parameters = {
-    "gatewayresponse.header.Access-Control-Allow-Origin" = "'*'" # replace with hostname of frontend (CloudFront)
-  }
-}
-
-resource "aws_api_gateway_gateway_response" "response_5xx" {
-  rest_api_id   = aws_api_gateway_rest_api.MyDemoAPI.id
-  response_type = "DEFAULT_5XX"
-
-  response_templates = {
-    "application/json" = "{'message':$context.error.messageString}"
-  }
-
-  response_parameters = {
-    "gatewayresponse.header.Access-Control-Allow-Origin" = "'*'" # replace with hostname of frontend (CloudFront)
-  }
+  # The "/*/*" portion grants access from any method on any resource
+  # within the API Gateway REST API.
+  source_arn = "${aws_api_gateway_rest_api.crc_api.execution_arn}/*/*"
 }
 
 
-resource "aws_api_gateway_deployment" "example" {
-  rest_api_id = aws_api_gateway_rest_api.MyDemoAPI.id
-lifecycle {
-    create_before_destroy = true
-  }
-    depends_on = [
-      aws_api_gateway_method.MyDemoMethod,
-      aws_api_gateway_integration.integration,
-      aws_api_gateway_integration_response.MyDemoIntegrationResponse
-    ]
-}
-
-
-resource "aws_api_gateway_stage" "example" {
-  deployment_id = aws_api_gateway_deployment.example.id
-  rest_api_id   = aws_api_gateway_rest_api.MyDemoAPI.id
-  stage_name    = "example"
-}
-
-
-output "crc_rest_api_execution_arn"{
-    value = aws_api_gateway_rest_api.MyDemoAPI.execution_arn
-}
-
-output api_gateway_stage_details {
-    value = {
-        "stage_name" = "example",
-        "stage_url" = "${aws_api_gateway_stage.example.invoke_url}/${aws_api_gateway_resource.MyDemoResource.path_part}"
-    }
+output "base_url" {
+  value = aws_api_gateway_deployment.crc_api.invoke_url
 }
